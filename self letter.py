bd2ce6dcf46f80fe0e58eba48ab0a0546a9fae74
ABDULLAH AL RAIHAN <mat1904057brur@gmail.com>
2026-08-11 11:33:02 +0600

    Add files via upload

---
+++ self letter.py
@@ -0,0 +1,11 @@
+Date=input("Enter the date:")
+Name=input("Enter your name:")
+Intrest=input("What are you interested in?\n=")
+print(Date)
+print("Dear",Name,",")
+print("I am proud of you. Keep learning. Keep growing.")
+if Intrest == "programming":
+  print("Every line of code you write today is taking you one step closer to the person you want to become.")
+else:
+  print("Whatever you choose to pursue, every day you keep learning and improving brings you closer to your best self")
+print("One day, your knowledge and hard work will allow you to make a meaningful contribution to society.Dear self, you were not born just to play a role in someone else’s story.You were born to create your own story, to lead, and to make an impact.\nRemember, who you are today is the result of your past struggles, sacrifices, failures, and hard work. Never forget your past self—the one who kept going even when things were difficult.\nNo matter what happens in your life, remember where you came from. Your past self is always a part of you, and your future self is waiting for you.\nKeep trying. Keep learning. Keep moving forward.One day, look back and make your past self proud of how far you came.You are not there yet—but you are getting closer every day.\nDear self, don't give up. Your story has only just begun.❤️\n")
